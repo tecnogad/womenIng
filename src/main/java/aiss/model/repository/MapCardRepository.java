cfe915334b9900eb92bb62531b7b097481dda981
CarmenJara <carjaralv@alum.us.es>
2019-05-04 13:30:32 +0200

    repository

--- src/main/java/aiss/model/repository/MapCardRepository.java
+++ src/main/java/aiss/model/repository/MapCardRepository.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import org.w3c.dom.ls.LSOutput;
 
 import aiss.model.Card;
-import aiss.model.Card; 
+ 
 import aiss.model.Song;
 
 
@@ -70,17 +70,22 @@ public class MapCardRepository implements CardRepository{
 //		gotye.setAlbum("Making Mirrors");
 //		addSong(gotye);
 //		
-//		// Create cards
-//		Card jacard=new Card();
-//		jacard.setName("AISSPlayList");
-//		jacard.setDescription("AISS PlayList");
-//		addCard(jacard);
-//		
-//		Card card = new Card();
-//		card.setName("Favourites");
-//		card.setDescription("A sample card");
-//		addCard(card);
-//		
+		// Create cards
+		Card agcard=new Card();
+		agcard.setName("Adele Goldberg");
+		agcard.setIntroduction("Empresaria e informática que participó en el desarrollo del lenguaje de programación Smaltalk-80,"
+				+ "el cual se utilizó como prototipo de la interfaz WIMP(ventanas, iconos, menús y apuntadores),"
+				+ "los inicios de las interfaces gráficas modernas.");
+		agcard.setImage("https://i.imgur.com/AzftjHt");
+		agcard.setQr("https://i.imgur.com/Frb7mtP");
+		addCard(agcard);
+		
+		Card lsCard = new Card();
+		lsCard.setName("Lucy Sanders");
+		lsCard.setIntroduction("Es la CEO actual del National Center for Women & Information Technology (NCWIT). \r\n" + 
+				"Lucy recibió el Bob Newman Lifetime Achievement Award por su tecnología innovadora");
+		addCard(lsCard);
+		
 //		// Add songs to cards
 //		addSong(jacard.getId(), rollingInTheDeep.getId());
 //		addSong(jacard.getId(), one.getId());
@@ -94,7 +99,7 @@ public class MapCardRepository implements CardRepository{
 	// Card related operations
 	@Override
 	public void addCard(Card p) {
-		String id = "p" + index++;	
+		String id = "c" + index++;	
 		p.setId(id);
 		cardMap.put(id,p);
 	}
